ba8c6f02860eb66050f5968d654cd789a185e4dd
Merzak Benaissi <merzak89merzak@gmail.com>
2024-03-25 17:20:44 +0100

    secure the database with the .env

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config({path:"../.env"})
 const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
 
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb+srv://merzaak:16111989@cluster0.pqoqaay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true })
+    await mongoose.connect(process.env.DB_USER_PASS, { useNewUrlParser: true })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
